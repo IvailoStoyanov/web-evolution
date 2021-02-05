b0cbf4558c5dc8574b9163ec9933431032b0bc64
Ivaylo Stoyanov Stoyanov <ivailo.stoyanov.work@gmail.com>
2021-02-05 18:04:02 +0200

    test examplery form

--- we-dev-env/components/survey-popup-form/SurveyPopupForm.tsx
+++ we-dev-env/components/survey-popup-form/SurveyPopupForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styles from "./SurveyPopupForm.module.scss";
 
 interface SurveyPopupFormProps {
@@ -6,7 +6,6 @@ interface SurveyPopupFormProps {
 }
 
 class SurveyPopupForm extends React.Component<SurveyPopupFormProps> {
-  
   handleClick = () => {
     this.props.toggle();
   };
@@ -18,14 +17,15 @@ class SurveyPopupForm extends React.Component<SurveyPopupFormProps> {
           <div className={styles.intro}>Get in touch</div>
           <h2>Just fill in the form and we will be in touch.</h2>
 
-          <form
-            name="contact"
+          {/* <form
+            name="survey"
             action="/success"
             method="POST"
             data-netlify-honeypot="bot-field"
-            data-netlify="true"
+            netlify
+            enctype="application/x-www-form-urlencoded"
           >
-            <input type="hidden" name="form-name" value="contact" />
+            <input type="hidden" name="form-name" value="survey" />
             <p>
               <label htmlFor="name">Name</label>
               <input type="text" id="name" name="name" required />
@@ -36,12 +36,7 @@ class SurveyPopupForm extends React.Component<SurveyPopupFormProps> {
             </p>
             <p>
               <label htmlFor="email">Email</label>
-              <input
-                type="text"
-                name="email"
-                id="email"
-                required
-              />
+              <input type="text" name="email" id="email" required />
             </p>
             <p>
               <label htmlFor="company">Company</label>
@@ -49,12 +44,7 @@ class SurveyPopupForm extends React.Component<SurveyPopupFormProps> {
             </p>
             <p>
               <label htmlFor="phone">Phone number</label>
-              <input
-                type="number"
-                name="phone"
-                id="phone"
-                required
-              />
+              <input type="number" name="phone" id="phone" required />
             </p>
             <label className={styles.privacyPolicy}>
               <input type="checkbox" />
@@ -66,7 +56,62 @@ class SurveyPopupForm extends React.Component<SurveyPopupFormProps> {
               className={styles.submitButton}
               type="submit"
               value="Send form"
+              name="submit"
             ></input>
+          </form> */}
+          <form
+            name="contactForm"
+            method="POST"
+            action="/"
+            data-netlify-honeypot="bot-field"
+            data-netlify="true"
+            id="contact-form"
+            className="contact-form"
+          >
+            <p className="screen-reader-text">
+              <label>
+                Don't fill this out if you're human: <input name="bot-field" />
+              </label>
+            </p>
+            <p className="form-row">
+              <label htmlFor="contact-form-name" className="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="contact-form-name"
+                className="form-input"
+              />
+            </p>
+            <p className="form-row">
+              <label htmlFor="contant-form-email" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="contant-form-email"
+                className="form-input"
+              />
+            </p>
+            <p className="form-row">
+              <label htmlFor="contant-form-message" className="form-label">
+                Message
+              </label>
+              <textarea
+                name="message"
+                id="contant-form-message"
+                className="form-textarea"
+                rows="7"
+              />
+            </p>
+            <input type="hidden" name="form-name" value="contactForm" />
+            <p className="form-row form-submit">
+              <button type="submit" className="button">
+                Send Message
+              </button>
+            </p>
           </form>
 
           <div className={styles.closeSurveyIcon}>

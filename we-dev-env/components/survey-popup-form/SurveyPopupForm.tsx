2e66eebd89ad5f97df0239989bf7ed69611ed867
Ivaylo Stoyanov Stoyanov <ivailo.stoyanov.work@gmail.com>
2021-02-05 17:40:57 +0200

    adding IDs

--- we-dev-env/components/survey-popup-form/SurveyPopupForm.tsx
+++ we-dev-env/components/survey-popup-form/SurveyPopupForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./SurveyPopupForm.module.scss";
 
 interface SurveyPopupFormProps {
@@ -6,10 +6,7 @@ interface SurveyPopupFormProps {
 }
 
 class SurveyPopupForm extends React.Component<SurveyPopupFormProps> {
-  state = {
-    seen: true,
-  };
-
+  
   handleClick = () => {
     this.props.toggle();
   };
@@ -20,28 +17,44 @@ class SurveyPopupForm extends React.Component<SurveyPopupFormProps> {
         <div className={styles.wrapper}>
           <div className={styles.intro}>Get in touch</div>
           <h2>Just fill in the form and we will be in touch.</h2>
-        
-            <form name="contact" action="/form-success" method="POST" data-netlify="true">
+
+          <form
+            name="contact"
+            action="/success"
+            method="POST"
+            data-netlify-honeypot="bot-field"
+            data-netlify="true"
+          >
             <input type="hidden" name="form-name" value="contact" />
             <p>
               <label htmlFor="name">Name</label>
-              <input type="text" name="name" required />
+              <input type="text" id="name" name="name" required />
             </p>
             <p>
               <label htmlFor="surname">Last Name</label>
-              <input type="text" name="surname" />
+              <input type="text" id="surname" name="surname" />
             </p>
             <p>
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" />
+              <input
+                type="text"
+                name="email"
+                id="email"
+                required
+              />
             </p>
             <p>
               <label htmlFor="company">Company</label>
-              <input type="text" name="company" />
+              <input type="text" name="company" id="company" />
             </p>
             <p>
               <label htmlFor="phone">Phone number</label>
-              <input type="number" name="phone" />
+              <input
+                type="number"
+                name="phone"
+                id="phone"
+                required
+              />
             </p>
             <label className={styles.privacyPolicy}>
               <input type="checkbox" />
